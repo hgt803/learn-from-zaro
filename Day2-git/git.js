86500640ce502d5f1176859330f519742976cb66
cake <hgt803@gmail.com>
2022-05-13 00:28:01 +0800

    delete excess

--- Day2-git/git.js
+++
@@ -1,9 +0,0 @@
-/**
- $ @Author: cake
- $ @Date: 2022-05-10 21:55:09
- $ @LastEditTime: 2022-05-10 21:55:10
- $ @LastEditors: cake
- $ @FilePath: /learn-from-zaro/Day2-git/git.js
- */
-
- edit git.js  make modified 
